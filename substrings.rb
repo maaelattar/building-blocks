737834b48017f60ce939b13ae4e1dcf97bb0e971
mohamed elattar <maaelattar@gmail.com>
2018-09-09 00:10:23 +0200

    Solve Substrings

---
+++ substrings.rb
@@ -0,0 +1,19 @@
+def substrings(sentence, dict)
+  hash = Hash.new(0)
+
+  str_arr = sentence.downcase.split(' ')
+
+  str_arr.each do |word|
+    dict.each do |substr|
+      hash[substr] += 1 if word.include?(substr)
+    end
+  end
+
+  puts hash
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+substrings('below', dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
